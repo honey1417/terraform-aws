e03e7e41e4d11cac5fd0ee0309bb017e51ef5f2b
harshini <harshinipamarthi@gmail.com>
2025-07-29 14:44:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,39 @@
+# Fetch existing key pair
+
+data "aws_key_pair" "key_pair" {
+  key_name = "ssh-key-pair-1"  # 👈 Replace with your key pair name
+}
+
+#to enable ssh access, security group should be created
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Restrict to your IP!
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+#EC2 instance 
+
 resource "aws_instance" "web" {
   ami           = "ami-05f991c49d264708f"
   instance_type = "t2.micro"
-  key_name = ""
+  key_name      = data.aws_key_pair.key_pair.key_name  # Reference the key
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
   tags = {
-    Name = "Hello1"
+    Name = "test-instance"
   }
 }
